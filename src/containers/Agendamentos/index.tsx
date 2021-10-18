e449c513c8447b7c4daaa95949fc2b3cd9374b01
Camilla Correia <camilla.correia@picpay.com>
2021-10-18 20:50:02 -0300

    feat: criação dos containers

---
+++ src/containers/Agendamentos/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { observer } from 'mobx-react';
+
+function AgendamentosComp() {
+  return (
+    <>
+      <h1>Agendamentos</h1>
+    </>
+  );
+}
+
+export const Login = observer(AgendamentosComp);
---
+++ src/containers/Audios/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { observer } from 'mobx-react';
+
+function AudiosComp() {
+  return (
+    <>
+      <h1>Audios</h1>
+    </>
+  );
+}
+
+export const Login = observer(AudiosComp);
